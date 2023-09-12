f6d72513acdda4fe157d46867393211714051537
Nikita <nrw@narola.email>
2023-09-12 14:39:10 +0530

    Changes in properties

--- src/main/java/com/example/profileannotation/NewActiveProfile/MyController.java
+++ src/main/java/com/example/profileannotation/NewActiveProfile/MyController.java
@@ -1,6 +1,5 @@
 package com.example.profileannotation.NewActiveProfile;
 
-import com.example.profileannotation.ActiveProfile.MessageController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
