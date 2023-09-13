31c199b3d7024afe347d3681acdca6b6b9dc5e2c
Nikita <nrw@narola.email>
2023-09-13 10:32:28 +0530

    Changes in properties

--- src/main/java/com/example/profileannotation/NewActiveProfile/MyController.java
+++ src/main/java/com/example/profileannotation/NewActiveProfile/MyController.java
@@ -18,14 +18,15 @@ public class MyController {
     @Value("${production.property5}")
     private String productionProperty5;
 
-
     @Value("${app.message}")
     private  String developmentProperty;
 
     @GetMapping("/properties")
     public String getProperties() {
         Logger logger= LoggerFactory.getLogger(MyController.class);
+        logger.error("Error from controller....!!!");
         logger.info("Hello i am from Controller");
+        logger.warn("Hello i am from warn");
         return "Common Property 1: " + commonProperty1 +
                 "\nLocal Property 3: " + localProperty3 +
                 "\nProduction Property 5: " + productionProperty5
